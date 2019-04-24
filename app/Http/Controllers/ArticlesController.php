edad52caee55dd1fe3020060ee7116366753b92b
akim157 <akim157@mail.ru>
2019-04-24 19:13:09 +0300

    v1.5.1

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,9 +3,21 @@
 namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\PortfoliosRepository;
 
-class ArticlesController extends Controller
+
+class ArticlesController extends SiteController
 {
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep)
+    {
+        parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
+
+        $this->p_rep = $p_rep;
+        $this->a_rep = $a_rep;
+        $this->bar = 'right';
+        $this->template = env('THEME').'.articles';
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +25,21 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+
+        $articles = $this->getArticles();
+        $content = view(env('THEME').'.articles_content')->with('articles', $articles)->render();
+        $this->vars['content'] = $content;
+        return $this->renderOutput();
+    }
+
+    public function getArticles($alias = false)
+    {
+        $articles = $this->a_rep->get(['title', 'alias', 'created_at', 'img', 'desc', 'user_id', 'category_id', 'id'], false, true);
+        if($articles)
+        {
+            $articles->load('user', 'category', 'comments');
+        }
+        return $articles;
     }
 
     /**
--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -7,11 +7,15 @@ abstract class Repository
 {
     protected $model = false; //объект модели получаемых данных
 
-    public function get($select = '*', $take = false){
+    public function get($select = '*', $take = false, $pagination = false) {
         $builder = $this->model->select($select);
         if($take)
         {
             $builder->take($take);
+        }
+        if($pagination)
+        {
+            return $this->check($builder->paginate(Config::get('settings.paginate')));
         }
         return $this->check($builder->get());
     }
@@ -19,7 +23,7 @@ abstract class Repository
     protected function check($result)
     {
         if($result->isEmpty()) return false;
-        $result->transform(function($item,$key){
+        $result->transform(function($item,$key) {
             if(is_string($item->img) && is_object(json_decode($item->img)) && json_last_error() == JSON_ERROR_NONE) {
                 $item->img = json_decode($item->img);
             }
